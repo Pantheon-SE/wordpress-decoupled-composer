3739c28dc6bad273651510dae6890c8b5d29f561
Kyle Taylor <kyletaylored@gmail.com>
2022-08-12 15:28:22 -0500

    Test ocean

--- web/wp-import.php
+++ web/wp-import.php
@@ -1,7 +1,7 @@
 <?php
 
 require '../vendor/autoload.php';
-require 'index.php';
+require 'app/plugins/ocean-extra/ocean-extra.php';
 
 $demos = OceanWP_Demos::get_demos_data();
 $categories = OceanWP_Demos::get_demo_all_categories( $demos );
