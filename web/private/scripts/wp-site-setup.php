110181b8fea0083c18408773a47bf30a2b65a945
Kyle Taylor <kyletaylored@gmail.com>
2022-08-16 12:02:31 -0700

    Add oceanwp

--- web/private/scripts/wp-site-setup.php
+++ web/private/scripts/wp-site-setup.php
@@ -1,7 +1,8 @@
 <?php
 
-echo "Enabling WP plugins...\n";
+echo "Enabling WP plugins and themes...\n";
 passthru("wp plugin activate pantheon-advanced-page-cache wp-native-php-sessions pantheon-se-plugin wp-graphql");
+passthru("wp theme install oceanwp --activate");
 
 echo "Setting Permalink structure... \n";
 passthru("wp rewrite structure /%year%/%monthnum%/%day%/%postname%/");
