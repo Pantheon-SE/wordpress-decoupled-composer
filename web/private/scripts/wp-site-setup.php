a5a66c59161c83d5eb4ec47a4df23161beba81a3
Kyle Taylor <kyletaylored@gmail.com>
2022-08-16 11:50:49 -0700

    Enable WPGraphQL

--- web/private/scripts/wp-site-setup.php
+++ web/private/scripts/wp-site-setup.php
@@ -1,7 +1,7 @@
 <?php
 
 echo "Enabling WP plugins...\n";
-passthru("wp plugin activate pantheon-advanced-page-cache wp-native-php-sessions pantheon-se-plugin");
+passthru("wp plugin activate pantheon-advanced-page-cache wp-native-php-sessions pantheon-se-plugin wp-graphql");
 
 echo "Setting Permalink structure... \n";
 passthru("wp rewrite structure /%year%/%monthnum%/%day%/%postname%/");
